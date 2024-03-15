86171a36ebf0edf86036c64c1382a9ed4a4090f5
adityakr03 <leo03.akrs@gmail.com>
2024-03-16 00:23:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -29,3 +29,20 @@ function convert() {
     // Set color based on the gradient
     result.style.color = 'rgb(' + red + ',0,' + blue + ')';
 }
+// Background Color change function
+function generateGradientColor() {
+    // Generate random RGB color values
+    let r1 = Math.floor(Math.random() * 256);
+    let g1 = Math.floor(Math.random() * 256);
+    let b1 = Math.floor(Math.random() * 256);
+    let r2 = Math.floor(Math.random() * 256);
+    let g2 = Math.floor(Math.random() * 256);
+    let b2 = Math.floor(Math.random() * 256);
+
+    // Construct CSS gradient string
+    let gradientColor = `linear-gradient(to right, rgb(${r1}, ${g1}, ${b1}), rgb(${r2}, ${g2}, ${b2}))`;
+
+    // Set body background to gradient color
+    document.body.style.background = gradientColor;
+}
+generateGradientColor();
